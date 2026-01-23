8d85a2f2f6eba52542bec5af431e2c9aad3af2bb
Lennart Breede <lennart.breede@gmail.com>
2026-01-22 22:31:12 -0800

    improved part 2

--- rust/2018/src/bin/01.rs
+++ rust/2018/src/bin/01.rs
@@ -40,21 +40,21 @@ fn main() -> Result<()> {
     fn part2<R: BufRead>(reader: R) -> Result<i32> {
         let changes: Vec<i32> = reader
             .lines()
-            .map(|line| line.unwrap().parse::<i32>().unwrap())
-            .collect();
+            .map(|l| Ok(l?.parse()?))
+            .collect::<Result<_>>()?;
 
-        let mut frequencies: HashSet<i32> = HashSet::new();
-        let mut frequency = 0;
-        frequencies.insert(frequency);
-        for i in 0.. {
-            let change = changes[i % changes.len()];
-            frequency += change;
-            if frequencies.contains(&frequency) {
-                return Ok(frequency);
+        let mut seen = HashSet::new();
+        let mut freq = 0;
+        seen.insert(freq);
+
+        for change in changes.iter().cycle() {
+            freq += change;
+            if !seen.insert(freq) {
+                return Ok(freq);
             }
-            frequencies.insert(frequency);
         }
-        Err(anyhow!("no duplicate frequency found"))
+
+        unreachable!("cycle() is infinite unless input is empty");
     }
 
     assert_eq!(2, part2(BufReader::new(EXAMPLE.as_bytes()))?);
